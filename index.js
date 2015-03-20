5275ecd6dd7181e2c281da7b27f970bf35af7a9e
Sebastian Porto <sebasporto@gmail.com>
2015-03-20 14:44:09 +1100

    WIP

---
+++ index.js
@@ -0,0 +1,42 @@
+// var r = require('rethinkdb');
+
+// r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
+//   if(err) throw err;
+
+//   // r.db('test1').tableCreate('robots').run(conn, function(err, res) {
+//   //   if(err) throw err;
+//   //   console.log(res);
+    
+//   //   r.table('robots').insert({ name: 'Rotty' }).run(conn, function(err, res)
+//   //   {
+//   //     if(err) throw err;
+//   //     console.log(res);
+//   //   });
+//   // });
+
+
+//    // r.db('test1').table('robots').insert({ name: 'Rotty' }).run(conn, function(err, res)
+//    //  {
+//    //    if(err) throw err;
+//    //    console.log(res);
+//    //  });
+
+//   //changes
+
+//   r.db('test1').table('robots').changes().run(conn, function(err, cursor) {
+//     cursor.each(console.log);
+//   });
+// });
+
+var feathers   = require('feathers');
+var bodyParser = require('body-parser');
+
+var app = feathers();
+var todoService = require('./todos');
+
+app.configure(feathers.rest())
+  .configure(feathers.socketio())
+  .use(bodyParser.json())
+  .use('/todos', todoService)
+  .use('/', feathers.static(__dirname))
+  .listen(3000);
